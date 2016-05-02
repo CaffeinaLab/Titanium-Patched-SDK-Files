05c307854a4e0b0cefa62006f1a7da53acf6cb4a
Flavio De Stefano <flavio.destefano@caffeina.com>
2016-05-02 14:32:11 +0200

    Fix

--- patch.sh
+++ patch.sh
@@ -6,7 +6,7 @@ SDKPATH=~/Library/Application\ Support/Titanium/mobilesdk/osx
 for s in $(ls "$CURPATH/sdk"); do
 	if [ -d "$SDKPATH/$s" ]; then
 		echo "SDK $s patched!"
-		cp -R "$CURPATH/sdk/$s" "$SDKPATH/$s"
+		cp -R "$CURPATH/sdk/$s/" "$SDKPATH/$s"
 	else
 		echo "SDK $s is not installed"
 	fi
